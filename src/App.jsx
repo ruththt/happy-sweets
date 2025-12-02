d7158793ba09d776de0c47cbcc44a9b3a08dac7f
ruththt <rutaa987@gmail.com>
2025-12-03 01:01:21 +0300

    Initial upload of Happy Sweets project

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div><h1>HAPPY SWEETS</h1>
+    <h2>Here are your delicious Desserts and Drinks </h2>
+    <p>Delicious sweets made fresh every day! 🎉</p>
+      <h3>🍪 Sweets Menu  </h3>
+      <h3>Sweets 🍩 🍰</h3>
+      <ul><li>cookies</li>
+        <li>donuts</li>
+        <li>cream cake</li>
+        <li>soft cake</li></ul>
+        <h3> Juices 🍓 🥤</h3>
+        <ul><li>Avocado</li>
+        <li>Mango</li>
+        <li>Pineapple</li>
+        <li>Strawberry</li></ul>
+        <h2>Our Special 😋🍮</h2>
+        <ul>Chocolate Lava</ul>
+        <ul>Caramel Milkshake <h6>with chocolate and vanilla</h6></ul>
+       </div>
+          
+  )
+ 
+}
+export default App;
